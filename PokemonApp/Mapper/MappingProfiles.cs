e7ef2d0d643c997a88256d77a7085e8f53ce9a03
danganhphu <daphu.dev@gmail.com>
2023-08-21 22:36:20 +0700

    Add: Mapping Profiles

---
+++ PokemonApp/Mapper/MappingProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using PokemonApp.Models;
+using PokemonReviewApp.Dto;
+
+namespace PokemonApp.Mapper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Country, CountryDto>();
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<Review, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
+            CreateMap<CategoryDto, Category>();
+            CreateMap<CountryDto, Country>();
+            CreateMap<OwnerDto, Owner>();
+            CreateMap<PokemonDto, Pokemon>();
+            CreateMap<ReviewDto, Review>();
+            CreateMap<ReviewerDto, Reviewer>();
+        }
+    }
+}
