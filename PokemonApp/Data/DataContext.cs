c1c58bb2d6adefca592f616b766c97b830d33070
danganhphu <daphu.dev@gmail.com>
2023-08-21 21:41:14 +0700

    Add: model and ef core

---
+++ PokemonApp/Data/DataContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonApp.Models;
+
+namespace PokemonApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //flutent api cho PokemonCategory
+            modelBuilder.Entity<PokemonCategory>(pCategory =>
+            {
+                pCategory.HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+
+                pCategory.HasOne(p => p.Pokemon)
+                    .WithMany(pc => pc.PokemonCategories)
+                    .HasForeignKey(p => p.PokemonId);
+
+                pCategory.HasOne(p => p.Category)
+                    .WithMany(pc => pc.PokemonCategories)
+                    .HasForeignKey(c => c.CategoryId);
+            });
+
+            //flutent api cho PokemonOwner
+            modelBuilder.Entity<PokemonOwner>(pOwner =>
+            {
+                pOwner.HasKey(po => new { po.PokemonId, po.OwnerId });
+
+                pOwner.HasOne(p => p.Pokemon)
+                    .WithMany(po => po.PokemonOwners)
+                    .HasForeignKey(p => p.PokemonId);
+
+                pOwner.HasOne(o => o.Owner)
+                    .WithMany(po => po.PokemonOwners)
+                    .HasForeignKey(c => c.OwnerId);
+            });
+        }
+    }
+}
---
+++ PokemonApp/Utils/EnviConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonApp.Utils
+{
+    public class EnviConfig
+    {
+        public static string DevConnectionString { get; private set; }
+
+        public static void Config(IConfiguration configuration)
+            => DevConnectionString = configuration.GetConnectionString("DevConnection");
+    }
+}
