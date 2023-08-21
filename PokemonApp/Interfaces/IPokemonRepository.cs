0225d038e14f374951c0f25affad0941760bdca2
danganhphu <daphu.dev@gmail.com>
2023-08-21 22:35:18 +0700

    Add: IRepository

---
+++ PokemonApp/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,28 @@
+﻿using PokemonApp.Models;
+using PokemonReviewApp.Dto;
+
+namespace PokemonApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+
+        Pokemon GetPokemon(int id);
+
+        Pokemon GetPokemon(string name);
+
+        Pokemon GetPokemonTrimToUpper(PokemonDto pokemonCreate);
+
+        decimal GetPokemonRating(int pokeId);
+
+        bool PokemonExists(int pokeId);
+
+        bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon);
+
+        bool UpdatePokemon(int ownerId, int categoryId, Pokemon pokemon);
+
+        bool DeletePokemon(Pokemon pokemon);
+
+        bool Save();
+    }
+}
