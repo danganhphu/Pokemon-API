cdb311a858e91545d932793801ff8e559f417c81
danganhphu <daphu.dev@gmail.com>
2023-08-21 22:35:49 +0700

    Add: Repository pattern

---
+++ PokemonApp/Repository/CategoryRepository.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using PokemonApp.Data;
+using PokemonApp.Interfaces;
+using PokemonApp.Models;
+
+namespace PokemonApp.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(c => c.Id == id);
+        }
+
+        public bool CreateCategory(Category category)
+        {
+            _context.Add(category);
+            return Save();
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            _context.Remove(category);
+            return Save();
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(e => e.CategoryId == categoryId).Select(c => c.Pokemon).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            _context.Update(category);
+            return Save();
+        }
+    }
+}
